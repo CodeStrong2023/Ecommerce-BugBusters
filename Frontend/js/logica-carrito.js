0c28e9859341a4340ac19fb37d979d95567e07a4
Nicolas Nuñez <nuneznicolas582@gmail.com>
2024-10-25 17:22:37 -0300

    Corrijo logica del carrito al borrar producto

--- Frontend/js/logica-carrito.js
+++ Frontend/js/logica-carrito.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
             productoDiv.innerHTML = `
                 <div class="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
                     <div class="rounded-lg md:w-2/3">
-                        <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start" data-product-id="${producto.id}">
+                        <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start" data-product-id="${producto.id}" id="producto">
                             <img src="http://localhost:8080${producto.imagenUrl}" alt="product-image" class="w-full rounded-lg sm:w-40" />
                             <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
                                 <div class="mt-5 sm:mt-0">
@@ -73,6 +73,12 @@ function updateTotal() {
     totalElement.textContent = `Total: $${total.toFixed(2)}`; // Actualizar el precio total en la interfaz
 }
 
+function eliminarProducto(productoid) {
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    const carritoActualizado = carrito.filter(item => item.id !== Number(productoid));
+    localStorage.setItem('carrito', JSON.stringify(carritoActualizado));
+}
+
 // Función para configurar oyentes de eventos para incrementar y decrementar
 function setupEventListeners() {
     const incrementButtons = document.querySelectorAll('.increment');
@@ -121,10 +127,15 @@ function setupEventListeners() {
     removeButtons.forEach(button => {
         button.addEventListener('click', function() {
             const productDiv = this.closest('.producto-agregado');
+            const producto = productDiv.querySelector("#producto")
+            const productoId = producto.getAttribute('data-product-id');
             if (productDiv) {
+                eliminarProducto(productoId)
                 productDiv.remove();
                 updateTotal(); // Actualizar el total general al eliminar un producto
             }
         });
     });
+
 }
+
